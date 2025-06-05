962034f842e3b8a8e9ef2faab8b2a0cdf1f54613
Omar-wassef <omarwassef1@gmail.com>
2025-06-05 14:54:37 +0300

    Implemented Auth, Billing, Database setup, Companion server Action

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,50 @@
+import CompanionCard from "@/components/CompanionCard"
+import CompanionsList from "@/components/CompanionsList"
+import CTA from "@/components/CTA"
+import { recentSessions } from "@/constants"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <main>
+      <h1>Popular companions</h1>
+
+      <section className="home-section">
+        {/* change these to the actual cards. */}
+        <CompanionCard 
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard 
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="Maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard 
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="Language"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList 
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import Navitems from "./Navitems"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
 
 const Navbar = () => {
   return (
@@ -13,7 +14,14 @@ const Navbar = () => {
 
         <div className="flex items-center gap-8">
             <Navitems />
-            <p>Sign in</p>
+            <SignedOut>
+              <SignInButton>
+                <button className="btn-signin">Sign in</button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
         </div>
     </nav>
   )
